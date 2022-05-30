1f1d74a84f02be47a86406a0339814a90b2683b9
rkdmf1026 <rkdmf1026@naver.com>
2022-05-31 02:59:18 +0900

    [ADD] okhttp Log

--- app/src/main/java/com/example/repositorysearchapp/network/RetrofitBuilder.kt
+++ app/src/main/java/com/example/repositorysearchapp/network/RetrofitBuilder.kt
@@ -1,13 +1,34 @@
 package com.example.repositorysearchapp.network
 
+import okhttp3.Interceptor
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitBuilder {
     private const val BASE_URL = "https://api.github.com/"
 
+    private val interceptor = HttpLoggingInterceptor().also {
+        it.level = HttpLoggingInterceptor.Level.BODY
+    }
+
+    private val headerInterceptor = Interceptor {
+        val request = it.request()
+            .newBuilder()
+            .addHeader("accept", "application/vnd.github.v3+jso")
+            .build()
+        return@Interceptor it.proceed(request)
+    }
+
+    private val client = OkHttpClient.Builder()
+        .addInterceptor(headerInterceptor)
+        .addInterceptor(interceptor)
+        .build()
+
     private val retrofit = Retrofit.Builder()
         .baseUrl(BASE_URL)
+        .client(client)
         .addConverterFactory(GsonConverterFactory.create())
         .build()
 

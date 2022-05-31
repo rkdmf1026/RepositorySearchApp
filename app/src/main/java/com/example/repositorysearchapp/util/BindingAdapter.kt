0199cf8de75038a101bde129dcf0686110c9dff2
rkdmf1026 <rkdmf1026@naver.com>
2022-05-31 15:39:30 +0900

    [ADD] KeyBoard enter event

--- app/src/main/java/com/example/repositorysearchapp/util/BindingAdapter.kt
+++ app/src/main/java/com/example/repositorysearchapp/util/BindingAdapter.kt
@@ -1,10 +1,17 @@
 package com.example.repositorysearchapp.util
 
+import android.content.Context
+import android.view.KeyEvent
+import android.view.inputmethod.EditorInfo
+import android.view.inputmethod.InputMethodManager
+import android.widget.EditText
+import android.widget.TextView
 import androidx.databinding.BindingAdapter
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.repositorysearchapp.main.GitHubAdapter
 import com.example.repositorysearchapp.main.MainViewModel
+import com.example.repositorysearchapp.network.repository.GitHubRepository
 
 @BindingAdapter("EndlessScroll")
 fun RecyclerView.setEndlessScroll(
@@ -17,12 +24,31 @@ fun RecyclerView.setEndlessScroll(
             val lastItemPosition =
                 (recyclerView.layoutManager as LinearLayoutManager).findLastCompletelyVisibleItemPosition()
             val itemCount = recyclerView.adapter?.itemCount ?: return
-            val itemType = recyclerView.adapter?.getItemViewType(itemCount - 1)
+            val itemType = recyclerView.adapter?.getItemViewType(itemCount - 1) ?: return
 
             if (lastItemPosition == itemCount - 1 && itemType == GitHubAdapter.TYPE_LOADING) {
                 (recyclerView.adapter as GitHubAdapter).deleteLastItem()
-                viewModel.searchItem()
+                viewModel.searchItem(GitHubRepository.PAGING)
             }
+        }
+    })
+}
+
+@BindingAdapter("viewModel", "context")
+fun EditText.setSearchItem(
+    viewModel: MainViewModel,
+    context: Context
+) {
+    this.setOnEditorActionListener(object : TextView.OnEditorActionListener {
+        override fun onEditorAction(v: TextView?, actionId: Int, event: KeyEvent?): Boolean {
+            if (actionId == EditorInfo.IME_ACTION_DONE) {
+                viewModel.searchItem(0)
+                val inputMethodManager =
+                    context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+                inputMethodManager.hideSoftInputFromWindow(this@setSearchItem.windowToken, 0)
+                return true
+            }
+            return false
         }
     })
 }
